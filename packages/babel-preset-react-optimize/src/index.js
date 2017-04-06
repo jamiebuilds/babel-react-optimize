061143cd13c753e035992c33c3bbd7783a8c1f5f
Sébastien Lorber <slorber@users.noreply.github.com>
2017-04-06 11:08:51 +0200

    Put constant-elements plugin last
    see https://github.com/babel/babel/issues/5325#issuecomment-292064058

--- packages/babel-preset-react-optimize/src/index.js
+++ packages/babel-preset-react-optimize/src/index.js
@@ -1,8 +1,8 @@
 module.exports = {
   plugins: [
-    require('babel-plugin-transform-react-constant-elements'),
     require('babel-plugin-transform-react-inline-elements'),
     require('babel-plugin-transform-react-remove-prop-types')['default'],
-    require('babel-plugin-transform-react-pure-class-to-function')
+    require('babel-plugin-transform-react-pure-class-to-function'),
+    require('babel-plugin-transform-react-constant-elements')
   ]
 };
